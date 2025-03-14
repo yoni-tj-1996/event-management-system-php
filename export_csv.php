5189b28e6048e1b08d7af88243e3356c62ce4bc5
Nadh116 <nadhiijemal503@gmail.com>
2025-03-15 02:29:02 +0300

    Add files via upload

--- export_csv.php
+++ export_csv.php
@@ -1,16 +1,24 @@
 <?php
-require 'db.php';
+require 'db.php'; 
 
 header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="events.csv"');
-
 $output = fopen('php://output', 'w');
+
 fputcsv($output, ['ID', 'Title', 'Description', 'Venue', 'Start Date', 'End Date']);
 
-$query = "SELECT * FROM events";
+$query = "SELECT id, title, description, venue, start_date, end_date FROM events"; // Make sure the column names match
 $result = mysqli_query($conn, $query);
 
-while ($row = mysqli_fetch_assoc($result)) {
-    fputcsv($output, $row);
+if ($result) {
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        fputcsv($output, $row); // Write data row to CSV
+    }
+} else {
+    echo "Error: " . mysqli_error($conn); 
 }
+
+mysqli_close($conn); 
+exit(); 
 ?>
