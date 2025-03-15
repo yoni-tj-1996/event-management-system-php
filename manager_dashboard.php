4b894e9b630f70352d0befce33202e6d2a99b6a1
Yohanis Tasfa <yohanistasfa1996@gmail.com>
2025-03-15 12:15:02 +0300

    Add files via upload

--- manager_dashboard.php
+++ manager_dashboard.php
@@ -7,7 +7,7 @@ require 'manager_dashboard_logic.php';
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Manager Dashboard</title>
+    <title>Manager  Dashboard</title>
     <link rel="stylesheet" href="styles.css" />
     <script src="scripts.php"></script>
 </head>
