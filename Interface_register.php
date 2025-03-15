327d07d358a4fbbc429089b2b1a27347e9a56bf3
Nadh116 <nadhiijemal503@gmail.com>
2025-03-15 04:23:42 +0300

    Add files via upload

--- Interface_register.php
+++ Interface_register.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<?php require 'header.php';  ?>
+
     <div class="container">
         <h2>Register</h2>
         <form action="register.php" method="POST">
